de77f0ab02f41d3b85261944ca8d54388e93b6f6
littlesekii <contact.sekii@gmail.com>
2026-03-14 13:05:26 -0300

    test: added unit tests for initial endpoints

---
+++ src/test/java/com/littlesekii/kanban_api/controller/MainControllerTest.java
@@ -0,0 +1,31 @@
+package com.littlesekii.kanban_api.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(MainController.class)
+public class MainControllerTest {
+    
+    private final MockMvc mockMvc;
+
+    public MainControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void shouldReturnOk() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api"))
+            .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void shouldReturnPongAndCorrectSchema() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/ping"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Pong🏓"));
+    }
+}
