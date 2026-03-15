2e61e1493c276d231665b47efe4f92ec4a40c5e9
littlesekii <contact.sekii@gmail.com>
2026-03-15 16:48:45 -0300

    chore: seed kanban database

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,4 @@ INSERT INTO fields (title, sort_order, board_id) VALUES ('To do', 1, 1);
 INSERT INTO fields (title, sort_order, board_id) VALUES ('In progress', 2, 1);
 INSERT INTO fields (title, sort_order, board_id) VALUES ('Done', 3, 1);
 INSERT INTO cards (title, sort_order, field_id) VALUES ('Finish Kanban API', 1, 1);
+INSERT INTO cards (title, sort_order, field_id) VALUES ('Finish Kanban Frontend', 2, 1);
