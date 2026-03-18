14a76650b7008b24f8474285b1071c3100a84b05
littlesekii <contact.sekii@gmail.com>
2026-03-17 23:14:51 -0300

    test: add test properties profile

--- src/test/java/com/littlesekii/kanban_api/controller/MainControllerTest.java
+++ src/test/java/com/littlesekii/kanban_api/controller/MainControllerTest.java
@@ -3,11 +3,13 @@ package com.littlesekii.kanban_api.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(MainController.class)
+@ActiveProfiles("test")
 public class MainControllerTest {
     
     private final MockMvc mockMvc;
